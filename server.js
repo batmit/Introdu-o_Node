9a32ce6621eb933dae882739733bbc91b91c475f
Daniel Matos Falcão <45146744+batmit@users.noreply.github.com>
2023-11-17 12:07:28 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+//utilizando fastify
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get("/", () => {
+    return 'Hello World'
+})
+
+server.get("/hello", () => {
+    return 'Hello Rocketseat'
+})
+
+server.get("/node", () => {
+    return 'Hello Node.Js'
+})
+
+
+server.listen({
+    port: 3333,
+})
+

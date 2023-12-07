c2558a8c7790a176616f25d475ddc11c57ca3cc1
Daniel Matos Falcão <45146744+batmit@users.noreply.github.com>
2023-12-07 11:38:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
 //utilizando fastify
+import {DatabaseMemory} from './database-memory.js'
 
 import { fastify } from 'fastify'
 
 const server = fastify()
 
-server.get("/", () => {
-    return 'Hello World'
+// estou chamando o database
+const database = new DatabaseabaseMemory()
+
+// o POST significa criar
+//Quando eu usar o post para chamar localhost:3333/videos eu estarei criando um novo vídeo
+
+server.post("/videos", (request, reply) => {
+
+    const { title, description, duration } = request.body()
+
+    database.create({
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(201).send()
+ })
+
+server.get("/videos", (request, reply) => {
+
+    const search = resquest.Query
+
+    const videos = database.list()
+
+    return videos 
+
 })
 
-server.get("/hello", () => {
-    return 'Hello Rocketseat'
+//o put publica um novo video específico
+// eu enviarei o id 
+server.put("/videos/:id", (request, reply) => {
+
+    const videoId = request.params.id
+
+    const { title, description, duration } = request.body()
+
+
+    database.update(videoId, {
+        title, 
+        description,
+        duration
+    })
+
+    return reply.status(204).send()
 })
 
-server.get("/node", () => {
+server.delete("/videos/:id", () => {
     return 'Hello Node.Js'
 })
 
